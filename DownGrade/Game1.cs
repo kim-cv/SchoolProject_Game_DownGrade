5cc8272ea7b9a9ae6189f88edd0c27e9c3b8d699
Kim Lindhardt <kim-cv@kim-cv.com>
2015-02-13 11:14:45 +0100

    Added rocket rotation

--- DownGrade/Game1.cs
+++ DownGrade/Game1.cs
@@ -146,7 +146,7 @@ namespace DownGrade
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
             // TODO: Add your drawing code here
             spriteBatch.Begin();
